756226f126c32443716b5f4805efe906bd82a6b5
afasanello <68906088+afasanello@users.noreply.github.com>
2020-08-21 11:04:40 -0300

    Pre entrega 2 (falta trabajo grupal)

--- js/login.js
+++ js/login.js
@@ -6,52 +6,65 @@ document.addEventListener("DOMContentLoaded", function(e){
 });
 
 document.getElementById("login").addEventListener("click", (e) => {
-    var err = false;
+    var errUser = false;
+    var errPass = false;
     var user = document.getElementById("user").value;
     var password = document.getElementById("password").value;
     var remember = document.getElementById("recordar").checked;
 
-    // Caja de mensaje de error
-    var msgBox = document.getElementById("msgBox");
-    var message = document.getElementById("message");
-    msgBox.style.display = "none";
-    message.innerHTML = "";
+    // Cajas de mensaje de error
+    var msgUserBox = document.getElementById("msgUserBox");
+    var msgPassBox = document.getElementById("msgPassBox");
+    var messageUser = document.getElementById("messageUser");
+    var messagePass = document.getElementById("messagePass");
+
+    msgUserBox.style.display = "none";
+    messageUser.innerHTML = "";
+    msgPassBox.style.display = "none";
+    messagePass.innerHTML = "";
 
     if(user == ""){
         // Chequea si el usuario está vacío.
-        err = true;
-        message.innerHTML += "<li>Ingrese un usuario.</li>";
+        errUser = true;
+        messageUser.innerHTML += "<li>Creo que falta el usuario. ¿Quién es usted?</li>";
     }else{
         if(user.length < 4){
             // Chequea si el usuario tiene menos de cuatro caracteres.
-            err = true;
-            message.innerHTML += "<li>El usuario tiene menos de 4 caracteres.</li>";
+            errUser = true;
+            messageUser.innerHTML += "<li>El usuario tiene menos de 4 caracteres.</li>";
         }
         var regex = new RegExp("^[a-zA-Z0-9_]+$");
         if(!regex.test(user)){
             // Chequea que el usuario no contenga caracteres no alfanuméricos.
-            err = true;
-            message.innerHTML += "<li>El usuario solo puede contener letras, números y _</li>";
+            errUser = true;
+            messageUser.innerHTML += "<li>Hay símbolos extraños que no puedo reconocer. :(</li>";
         }
     }
 
     if(password == ""){
         // Chequea si la contraseña está vacía.
-        err = true;
-        message.innerHTML += "<li>Ingrese una contraseña.</li>";
+        errPass = true;
+        messagePass.innerHTML += "<li>Necesito una contraseña para saber que usted es usted.</li>";
     }else if(password.length < 6){
         // Chequea si la contraseña tiene menos de seis caracteres.
-        err = true;
-        message.innerHTML += "<li>La contraseña tiene menos de 6 caracteres.</li>";
+        errPass = true;
+        messagePass.innerHTML += "<li>La contraseña es muy insegura, tiene menos de 6 caracteres.</li>";
     }
 
-    if(err){
+    if(errUser || errPass){
         // Si estamos parados acá, se encontró un error. Se previene que se envíe el formulario.
         e.preventDefault();
         
         //Muestra un mensaje de error, que desaparece a los tres segundos.
-        msgBox.style.display = "block";
-        setTimeout(() => {msgBox.style.display = "none"}, 3000);
+        if(errUser){
+            msgUserBox.style.display = "inline-block";
+            setTimeout(() => {msgUserBox.style.display = "none"}, 5000);
+        }
+
+        if(errPass){
+            msgPassBox.style.display = "inline-block";
+            setTimeout(() => {msgPassBox.style.display = "none"}, 5000);
+        }
     }else{
         //Se accede al inicio de sesión.
         if(remember){
--- js/products.js
+++ js/products.js
@@ -3,20 +3,12 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL)
-    .then((res) =>{
+    .then((res) => {
         if(res.status == "ok"){
             var prod = res.data;
             var list = document.getElementById("prods");
             var content = ``;
             for(var i = 0; i < prod.length; i++){
-                var badgeColor;
-                if(prod[i].soldCount >= 20){
-                    badgeColor = "success";
-                }else if(prod[i].soldCount < 20 && prod[i].soldCount > 1){
-                    badgeColor = "warning";
-                }else{
-                    badgeColor = "danger";
-                }
                 content += `
                     <tr>
                         <td>
@@ -27,12 +19,164 @@ document.addEventListener("DOMContentLoaded", function (e) {
                             <p>${prod[i].description}</p> 
                         </td>
                         <td>
-                            <h4><span class="badge badge-${badgeColor}">Quedan ${prod[i].soldCount} unidades</span></h4>
+                            <h4><span class="badge badge-info">Se vendieron ${prod[i].soldCount} unidades</span></h4>
                         </td>
                     </tr>`;
             }
             list.innerHTML = content;
-            console.log(prod);
         }
     });
 });
+
+// Función principal que filtra los productos por cualquier criterio.
+function filterProducts(){
+    var min = document.getElementById("pMin").value;
+    var max = document.getElementById("pMax").value;
+    var cost = document.getElementById("cost").value;
+    var relevance = document.getElementById("relevance").value;
+    var search = document.getElementById("search").value;
+
+    getJSONData(PRODUCTS_URL)
+    .then((res) => {
+        if(res.status == "ok"){
+            var prod = res.data;
+            var list = document.getElementById("prods");
+            var content = ``;
+
+            // Bandera booleana cuyo valor es true si el producto cumple con los criterios
+            // de filtro.
+            var filter;
+
+            if(cost == "asc")
+                // Ordena de forma ascendente
+                prod.sort(function(a, b){
+                    return a.cost - b.cost;
+                });
+            else if(cost == "desc")
+                // Ordena de forma descendente
+                prod.sort(function(a, b){
+                    return b.cost - a.cost;
+                });
+
+            if(relevance)
+                prod.sort(function(a, b){
+                    // Ordena descendentemente por cantidad de vendidos.
+                    return b.soldCount - a.soldCount;
+                });
+
+            if(search != "")
+                // Filtra por búsqueda, ya sea por nombre o por descripción, sin importar
+                // si se escribe en mayúsculas o minúsculas.
+                prod = prod.filter(a => {
+                    var desc = a.description.toLowerCase();
+                    var name = a.name.toLowerCase();
+
+                    var s = search.toLowerCase();
+
+                    return desc.indexOf(s) != -1 || name.indexOf(s) != -1;
+                });
+
+            for(var i = 0; i < prod.length; i++){
+                filter = true;
+
+                // Si el mínimo está definido, se aplica un criterio para el mínimo.
+                if(min !== "")
+                    filter = filter && prod[i].cost >= parseInt(min);
+
+                // Si el máximo está definido, se aplica un criterio para el máximo.
+                if(max !== "")
+                    filter = filter && prod[i].cost <= parseInt(max);
+
+                // Si la bandera booleana de filtro es verdadera, el producto cumple con
+                // los filtros. Se despliega el producto.
+                if(filter){
+                    var name = prod[i].name;
+                    var desc = prod[i].description;
+                    if(search != ""){
+                        var nAux = name.toLowerCase();
+                        var dAux = desc.toLowerCase();
+                        var sAux = search.toLowerCase();
+
+                        var nIndex = nAux.indexOf(sAux);
+                        var dIndex = dAux.indexOf(sAux);
+
+                        // Resalta la primera coincidencia en el nombre.
+                        if(nIndex != -1){
+                            name = name.substring(0, nIndex)
+                                + "<span style='background-color: yellow'>"
+                                + name.substring(nIndex, nIndex + search.length)
+                                + "</span>"
+                                + name.substring(nIndex + search.length, nAux.length);
+                        }
+
+                        // Resalta la primera coincidencia en la descripción.
+                        if(dIndex != -1){
+                            desc = desc.substring(0, dIndex)
+                                + "<span style='background-color: yellow'>"
+                                + desc.substring(dIndex, dIndex + search.length)
+                                + "</span>"
+                                + desc.substring(dIndex + search.length, dAux.length);
+                        }
+
+                    }
+                    content += `
+                        <tr>
+                            <td>
+                                <img class="img-thumbnail" src="${prod[i].imgSrc}" width="300" alt="${prod[i].description}">
+                            </td>
+                            <td style="padding: 20px;">
+                                <h4>${name} <span class="badge badge-info">${prod[i].currency} ${prod[i].cost}</span></h4>
+                                <p>${desc}</p> 
+                            </td>
+                            <td>
+                                <h4><span class="badge badge-info">Se vendieron ${prod[i].soldCount} unidades</span></h4>
+                            </td>
+                        </tr>`;
+                }
+
+            }
+            list.innerHTML = content;
+        }
+    });
+}
+
+// Ordena por costo. Define los parámetros para que se ejecute en la función principal.
+function orderByCost(){
+    document.getElementById("relevance").value = "";
+    document.getElementById("relevanceBtn").innerHTML = "Ordenar por relevancia";
+
+    if(document.getElementById("cost").value == "asc"){
+        document.getElementById("cost").value = "desc";
+        document.getElementById("costBtn").innerHTML = "Ordenar por precio ↓";
+    }else{
+        document.getElementById("cost").value = "asc";
+        document.getElementById("costBtn").innerHTML = "Ordenar por precio ↑";
+    }
+    
+    filterProducts();
+}
+
+// Ordena por relevancia. Define los parámetros para que se ejecute en la función principal.
+function orderByRelevance(){
+    document.getElementById("cost").value = "";
+    document.getElementById("relevance").value = "desc";
+    
+    document.getElementById("costBtn").innerHTML = "Ordenar por precio";
+    document.getElementById("relevanceBtn").innerHTML = "Ordenar por relevancia ↑";
+
+    filterProducts();
+}
+
+// Reinicia los valores del formulario, y muestra todos los productos sin criterios de búsqueda.
+function resetValues(){
+    document.getElementById("pMin").value = "";
+    document.getElementById("pMax").value = "";
+    document.getElementById("cost").value = "";
+    document.getElementById("relevance").value = "";
+    document.getElementById("search").value = "";
+    
+    document.getElementById("costBtn").innerHTML = "Ordenar por precio";
+    document.getElementById("relevanceBtn").innerHTML = "Ordenar por relevancia";
+
+    filterProducts();
+}
